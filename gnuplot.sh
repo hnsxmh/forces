f22943f108655fdcd468268cdfd6bc01297ecbfc
hnsxmh <34691304+hnsxmh@users.noreply.github.com>
2021-03-23 14:13:11 +0800

    update script

--- gnuplot.sh
+++ gnuplot.sh
@@ -1,6 +1,7 @@
-set title 'the pressure profile over the top surface and bottom surface of the airfoil NACA0012'
-set xlabel 'length(m)'
-set ylabel 'pressure(Pa)'
-plot "forces/4100/forces_top.dat" u 1:2 lc rgbcolor "#2B60DE" title 'top', "forces/4100/forces_bottom.dat" u 1:2 lc rgbcolor "#F62817" title 'bottom'
+set title 'Cp profile over the top surface and bottom surface of the airfoil NACA0012 (alpha = 10 degree, Re = 3mill)'
+set xlabel 'length(x/c)'
+set ylabel 'Cp'
+set yrange [2:-6]
+plot "forces/4105/forces_top.dat" u 1:2 lc rgbcolor "#2B60DE" title 'top', "forces/4105/forces_bottom.dat" u 1:2 lc rgbcolor "#F62817" title 'bottom'
 pause 1
 reread
